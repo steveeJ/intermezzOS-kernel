0f655b8c2acbb291a4bf1643efd47200b205d182
Stefan Junker <mail@stefanjunker.de>
2017-05-25 12:16:07 +0200

    track the time using ISR32 (PIT)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #![feature(lang_items)]
 #![feature(asm)]
 #![feature(naked_functions)]
-#![feature(core_intrinsics)]
+#![feature(abi_x86_interrupt)]
+#![feature(const_fn)]
 
 #![no_std]
 #![no_main]
@@ -20,14 +21,13 @@ extern crate x86;
 use x86::bits64::irq::IdtEntry;
 use x86::shared::io::{inb};
 
-use core::intrinsics;
-
 extern crate keyboard;
-
 extern crate pic;
 
 #[cfg(not(test))]
 pub mod panic;
+
+use spin::Mutex;
 
 #[macro_use]
 extern crate intermezzos;
@@ -35,6 +35,12 @@ extern crate intermezzos;
 lazy_static! {
     static ref CONTEXT: intermezzos::kernel::Context = intermezzos::kernel::Context::new();
 }
+
+
+static mut TICK_COUNTER: Mutex<u64> = Mutex::new(0);
+static mut UPTIME_SECONDS: u64 = 7290;
+const TICK_DIVISOR: u64 = 1 << 9;
+const TICK_FREQUENCY: u64 = (pic::PIT_BASE_FREQUENCY as u64)/TICK_DIVISOR;
 
 #[no_mangle]
 pub extern "C" fn kmain() -> ! {
@@ -44,9 +50,36 @@ pub extern "C" fn kmain() -> ! {
         panic!("omg GPF");
     });
 
+    let nanoseconds = 1000000/TICK_FREQUENCY;
+    kprintln!(CONTEXT, "Timer has {}ns accuracy.", nanoseconds);
+
+    pic::set_pit_divisor(0, TICK_DIVISOR as u16);
     // IRQ0 (0) on PIC1 (32), so IDT index is 32
     let timer = make_idt_entry!(isr32, {
         pic::eoi_for(32);
+
+        let mut tick_counter = unsafe {
+            let option_guard = TICK_COUNTER.try_lock();
+            if option_guard.is_none() {
+                kprintln!(CONTEXT, "error: isr32 called simultaniously");
+                return
+            }
+            let mut option_guard = option_guard.unwrap();
+            *option_guard += 1;
+            *option_guard
+        };
+
+        if (tick_counter % TICK_FREQUENCY) == 0 {
+            let mut seconds = unsafe {
+                UPTIME_SECONDS += 1;
+                UPTIME_SECONDS
+            };
+            // assert_eq!(tick_counter, seconds * TICK_FREQUENCY);
+            let hours = seconds / 3600;
+            let minutes =  (seconds % 3600) / 60;
+            seconds = (seconds % 3600) % 60;
+            kprintln!(CONTEXT, "[{:02}:{:02}:{:02}] {}", hours, minutes, seconds, tick_counter);
+        }
     });
 
     // Keyboard uses IRQ1 and PIC1 has been remapped to 0x20 (32); therefore
@@ -65,9 +98,13 @@ pub extern "C" fn kmain() -> ! {
     CONTEXT.idt.set_handler(32, timer);
     CONTEXT.idt.set_handler(33, keyboard);
 
-    CONTEXT.idt.enable_interrupts();
-
     kprintln!(CONTEXT, "Kernel initialized.");
 
-    loop { }
+    CONTEXT.idt.enable_interrupts();
+
+    loop {
+        unsafe { asm!("hlt") }
+    }
+
+    panic!("the loop was escaped")
 }
